ccd9be51f4cc63e1b3c0c828f19fdc87aff4a21c
Edin <edin.vatrich@gmail.com>
2025-10-14 22:16:43 +0200

    Added pizza image

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,80 @@
+'use client';
+
 import Pizza from '@/comps/Pizza';
+import { useEffect } from 'react';
 
 const data = [
-  { id: 1, name: 'pizza romana', price: '10$' },
-  { id: 2, name: 'pizza capricciosa', price: '15$' },
-  { id: 3, name: 'pizza americana', price: '12$' },
-  { id: 4, name: 'pizza quattro formaggi ', price: '16$' },
-  { id: 5, name: 'pizza bianca', price: '9$' },
-  { id: 6, name: 'pizza e fichi', price: '10$' },
-  { id: 7, name: 'pizza casalinga ', price: '17$' },
-  { id: 8, name: 'Pizza pugliese', price: '10$' },
-  { id: 9, name: 'pizza mozzarella', price: '10$' },
-  { id: 10, name: 'pizza fungi', price: '10$' },
+  {
+    id: 1,
+    name: 'pizza romana',
+    price: '10$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 2,
+    name: 'pizza capricciosa',
+    price: '15$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 3,
+    name: 'pizza americana',
+    price: '12$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 4,
+    name: 'pizza quattro formaggi ',
+    price: '16$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 5,
+    name: 'pizza bianca',
+    price: '9$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 6,
+    name: 'pizza e fichi',
+    price: '10$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 7,
+    name: 'pizza casalinga ',
+    price: '17$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 8,
+    name: 'Pizza pugliese',
+    price: '10$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 9,
+    name: 'pizza mozzarella',
+    price: '10$',
+    image: '/pizza.jpg',
+  },
+  {
+    id: 10,
+    name: 'pizza fungi',
+    price: '10$',
+    image: '/pizza.jpg',
+  },
 ];
 
 export default function Home() {
   return (
     <div className="bg-gray-50">
-      <h1 className="text-3xl font-bold underline text-center">Pizza Shop</h1>
+      <h1
+        className="text-3xl font-bold underline text-center"
+        onClick={() => alert('Test!')}
+      >
+        Pizza Shop
+      </h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, non ex
         in aliquam libero fugiat id. Neque, debitis facere? Consectetur
--- comps/Pizza.tsx
+++ comps/Pizza.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
+
 // Define prop type
 type PropType = {
   pizzaData: {
     id: number;
     name: string;
     price: string;
+    image: string;
   };
 };
 
@@ -13,6 +16,13 @@ const Pizza: React.FC<PropType> = ({ pizzaData }) => {
     <div className="border text-center">
       <h2 className="text-center">{pizzaData.name}</h2>
       <p className="text-center">Price: {pizzaData.price}</p>
+      <Image
+        className="text-center"
+        width={150}
+        height={150}
+        src={pizzaData.image as string}
+        alt={pizzaData.name}
+      />
     </div>
   );
 };
