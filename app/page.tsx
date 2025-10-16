2137f2cd6796fc41750ff152db1383e35774f366
Edin <edin.vatrich@gmail.com>
2025-10-16 20:03:56 +0200

    Added Pizza images and extra large responsivnes

--- app/page.tsx
+++ app/page.tsx
@@ -8,61 +8,61 @@ const data = [
     id: 1,
     name: 'pizza romana',
     price: '10$',
-    image: '/pizza.jpg',
+    image: '/pizza romana.jpg',
   },
   {
     id: 2,
     name: 'pizza capricciosa',
     price: '15$',
-    image: '/pizza.jpg',
+    image: '/pizza capricciosa.jpg',
   },
   {
     id: 3,
     name: 'pizza americana',
     price: '12$',
-    image: '/pizza.jpg',
+    image: '/pizza americana.jpg',
   },
   {
     id: 4,
-    name: 'pizza quattro formaggi ',
+    name: 'pizza quattro formaggi',
     price: '16$',
-    image: '/pizza.jpg',
+    image: '/pizza quattro formaggi.jpg',
   },
   {
     id: 5,
     name: 'pizza bianca',
     price: '9$',
-    image: '/pizza.jpg',
+    image: '/pizza bianca.jpg',
   },
   {
     id: 6,
     name: 'pizza e fichi',
     price: '10$',
-    image: '/pizza.jpg',
+    image: '/pizza e fichi.jpg',
   },
   {
     id: 7,
-    name: 'pizza casalinga ',
+    name: 'pizza casalinga',
     price: '17$',
-    image: '/pizza.jpg',
+    image: '/pizza casalinga.jpeg',
   },
   {
     id: 8,
-    name: 'Pizza pugliese',
+    name: 'pizza pugliese',
     price: '10$',
-    image: '/pizza.jpg',
+    image: '/pizza pugliese.jpg',
   },
   {
     id: 9,
     name: 'pizza mozzarella',
     price: '10$',
-    image: '/pizza.jpg',
+    image: '/pizza mozzarella.jpg',
   },
   {
     id: 10,
     name: 'pizza fungi',
     price: '10$',
-    image: '/pizza.jpg',
+    image: '/pizza fungi.png',
   },
 ];
 
@@ -76,7 +76,7 @@ export default function Home() {
         Pizza Shop
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center p-4">
         {data.map((pizza, index) => (
           <Pizza pizzaData={pizza} key={pizza.id} />
         ))}
