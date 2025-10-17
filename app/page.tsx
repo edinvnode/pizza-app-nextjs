7aaa01a419cba9319825a372bfcdf0f0d3664514
Edin <edin.vatrich@gmail.com>
2025-10-17 21:37:45 +0200

    Added Pizza names and double extra large responsivnes

--- app/page.tsx
+++ app/page.tsx
@@ -6,61 +6,61 @@ import { useEffect } from 'react';
 const data = [
   {
     id: 1,
-    name: 'pizza romana',
+    name: 'Pizza romana',
     price: '10$',
     image: '/pizza romana.jpg',
   },
   {
     id: 2,
-    name: 'pizza capricciosa',
+    name: 'Pizza capricciosa',
     price: '15$',
     image: '/pizza capricciosa.jpg',
   },
   {
     id: 3,
-    name: 'pizza americana',
+    name: 'Pizza americana',
     price: '12$',
     image: '/pizza americana.jpg',
   },
   {
     id: 4,
-    name: 'pizza quattro formaggi',
+    name: 'Pizza quattro formaggi',
     price: '16$',
     image: '/pizza quattro formaggi.jpg',
   },
   {
     id: 5,
-    name: 'pizza bianca',
+    name: 'Pizza bianca',
     price: '9$',
     image: '/pizza bianca.jpg',
   },
   {
     id: 6,
-    name: 'pizza e fichi',
+    name: 'Pizza e fichi',
     price: '10$',
     image: '/pizza e fichi.jpg',
   },
   {
     id: 7,
-    name: 'pizza casalinga',
+    name: 'Pizza casalinga',
     price: '17$',
     image: '/pizza casalinga.jpeg',
   },
   {
     id: 8,
-    name: 'pizza pugliese',
+    name: 'Pizza pugliese',
     price: '10$',
     image: '/pizza pugliese.jpg',
   },
   {
     id: 9,
-    name: 'pizza mozzarella',
+    name: 'Pizza mozzarella',
     price: '10$',
     image: '/pizza mozzarella.jpg',
   },
   {
     id: 10,
-    name: 'pizza fungi',
+    name: 'Pizza fungi',
     price: '10$',
     image: '/pizza fungi.png',
   },
@@ -76,7 +76,7 @@ export default function Home() {
         Pizza Shop
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1 place-items-center p-4">
         {data.map((pizza, index) => (
           <Pizza pizzaData={pizza} key={pizza.id} />
         ))}
