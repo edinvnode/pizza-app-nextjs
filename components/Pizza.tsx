61382a02b1018b82bd7400231dc4a6fddca82afd
Alen Garibovic <a.garibovic95@gmail.com>
2025-10-19 14:55:13 +0200

    Prettify

--- components/Pizza.tsx
+++ components/Pizza.tsx
@@ -1,7 +1,8 @@
-"use client";
+'use client';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useRef } from 'react';
+import { useState } from 'react';
+import Modal from '@/components/Modal';
 
 type PropType = {
   pizzaData: {
@@ -13,32 +14,55 @@ type PropType = {
 };
 
 const Pizza: React.FC<PropType> = ({ pizzaData }) => {
-
   const divRef = useRef<HTMLDivElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleBorder = (e: React.MouseEvent<HTMLDivElement>) => {
     if (!divRef.current) return;
-
-    if (e.type === "mouseenter") {
-      divRef.current.style.border = "2px solid grey";
-      divRef.current.style.opacity = "0.5";
-    } else if (e.type === "mouseleave") {
-      divRef.current.style.border = "2px solid transparent";
-      divRef.current.style.opacity = "1";
-    }
+    e.type === "mouseover" ? divRef.current.style.border = '2px solid grey' : divRef.current.style.border = '2px solid transparent';
   };
 
   return (
-    <div className="border cursor-pointer p-7 shadow-2xl" ref={divRef} onMouseEnter={handleBorder} onMouseLeave={handleBorder}>
-      <Link href={{pathname: '/about', query: { pizzaData: JSON.stringify(pizzaData)}}}>
-        <Image
-          className="ml-3"
-          width={150}
-          height={150}
-          src={pizzaData.image as string}
-          alt={pizzaData.name}
-        />
-      </Link>
+    <div
+      className="border p-7 shadow-2xl relative"
+      ref={divRef}
+      onMouseOver={handleBorder}
+      onMouseOut={handleBorder}
+    >
+      <Image
+        className="ml-3"
+        width={150}
+        height={150}
+        src={pizzaData.image as string}
+        alt={pizzaData.name}
+      />
+      <button className="absolute left-1/3 cursor-pointer" onClick={() => setIsOpen(true)}>
+        View More
+      </button>
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <p>
+          <strong>Name: </strong>
+          {pizzaData?.name ?? 'Pizza'}
+        </p>
+        <hr />
+        <p className="mt-2">
+          <strong>Price: </strong>
+          {pizzaData?.price ?? 'Price'}
+        </p>
+        <hr />
+        <p className="mt-2">
+          <strong>Description: </strong> Nam eu fringilla felis. Curabitur in magna molestie,
+          tristique massa nec, molestie lectus.{' '}
+        </p>
+
+        <button
+          onClick={() => setIsOpen(false)}
+          className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-900 mt-5 cursor-pointer"
+        >
+          Close
+        </button>
+      </Modal>
     </div>
   );
 };
