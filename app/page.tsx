4015b64fdb3945217e5d70d06f10bd9ef61a23eb
Alen Garibovic <a.garibovic95@gmail.com>
2025-10-21 20:59:53 +0200

    naming changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Pizza from "@/components/Card/Card";
+import Card from "@/components/Card/Card";
 import { useGetPizzasQuery } from "@/redux/api/pizzaApi";
 
 export type PizzaType = {
@@ -16,7 +16,7 @@ export default function Home() {
     <div className="bg-gray-50 min-h-screen flex items-center justify-center">
       <main className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 place-items-center p-4">
         {pizzaData?.map((p) => (
-          <Pizza key={p.id} pizzaData={p} />
+          <Card key={p.id} pizzaData={p} />
         ))}
       </main>
     </div>
