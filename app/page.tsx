ad61a9d08470f1ff65b41f9b01f7a9a5ff348403
Alen Garibović <35613986+agaribovic@users.noreply.github.com>
2025-10-31 17:41:21 +0100

    styled pizza buttons

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 
   return (
     <div className="bg-gray-50 min-h-screen flex items-center justify-center">
-      <main className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 place-items-center p-4">
+      <main className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 place-items-center p-4 mb-18">
         {sortedPizzas?.map((pizza) => (
           <Card key={pizza.id} pizzaData={pizza} />
         ))}
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -29,7 +29,7 @@ const Card: React.FC<PropType> = ({ pizzaData }) => {
 
   return (
     <div
-      className="border p-7 shadow-2xl relative"
+      className="border p-7 shadow-2xl relative flex flex-col items-center mt-10"
       ref={divRef}
       onMouseOver={handleBorder}
       onMouseOut={handleBorder}
@@ -45,26 +45,28 @@ const Card: React.FC<PropType> = ({ pizzaData }) => {
       ) : (
         <Spinner size={200} />
       )}
-      <button
-        className="absolute left-1/8 cursor-pointer"
-        onClick={() => dispatch(pizzaDetails(pizzaData))}
-      >
-        View More
-      </button>
+      <div className="flex gap-2 absolute top-75">
+        <button
+          className="cursor-pointer bg-yellow-300 text-red-700 font-bold px-4 py-2 rounded-lg shadow-md hover:bg-yellow-400 transition w-25 h-12 text-sm"
+          onClick={() => dispatch(pizzaDetails(pizzaData))}
+        >
+          🍕 View
+        </button>
 
-      <button
-        className="absolute left-1/2 cursor-pointer"
-        onClick={() => dispatch(pizzaEdit(pizzaData))}
-      >
-        Edit
-      </button>
+        <button
+          className="cursor-pointer bg-green-300 text-yellow-800 font-bold px-4 py-2 rounded-lg shadow-md hover:bg-green-400 transition w-25 h-12 text-sm"
+          onClick={() => dispatch(pizzaEdit(pizzaData))}
+        >
+          🧀 Edit
+        </button>
 
-      <button
-        className="absolute right-1/8 cursor-pointer"
-        onClick={async () => await deletePizza(pizzaData?.id).unwrap()}
-      >
-        Delete
-      </button>
+        <button
+          className="cursor-pointer bg-red-500 text-white font-bold px-4 py-2 rounded-lg shadow-md hover:bg-red-600 transition w-25 h-12 text-sm"
+          onClick={async () => await deletePizza(pizzaData?.id).unwrap()}
+        >
+          🗑️ Delete
+        </button>
+      </div>
 
       <Modal
         isModalOpen={modalType.value === "pizzaDetails"}
