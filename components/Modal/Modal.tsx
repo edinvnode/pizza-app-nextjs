c611a071dfb458a2451b7acb0e4c3a36f4e6ba05
Alen Garibovic <a.garibovic95@gmail.com>
2025-11-10 18:50:51 +0100

    opacity fix

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -18,7 +18,7 @@ export default function Modal({ isModalOpen, closeModal, children, title }: Moda
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="absolute inset-0 bg-black opacity-10" onClick={closeModal}></div>
+      <div className="absolute inset-0 bg-black opacity-5" onClick={closeModal}></div>
       <div className={`relative bg-white rounded-2xl shadow-lg p-6 z-10 text-xl ${modalType.value === "pizzaDetails" ? "w-full max-w-md" : ""}`}>
         <h1 className="text-3xl text-center font-bold text-gray-900 mb-5">{title}</h1>
         {children}
